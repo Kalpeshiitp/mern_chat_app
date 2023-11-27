69a160466ac0d8670c6acded6a6c71fa8500256f
Kalpeshiitp <kalpeshkumar267@gmail.com>
2023-11-28 01:38:15 +0530

    deleted the build

--- backend/server.js
+++ backend/server.js
@@ -4,38 +4,17 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const chatRoutes = require('./routes/chatRoutes')
 const messageRoutes = require('./routes/messageRoutes')
-const path = require('path');
 const cors = require('cors');
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 dotenv.config();
 connectDB();
 const app = express()
 app.use(express.json())
-// app.get('/', (req,res)=>{
-//     res.send("API is running");
-// })
+
 app.use(cors());
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes)
 app.use('/api/message', messageRoutes)
-
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
